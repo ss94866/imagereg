0e3965b281217f37d171ca1635a1b09dd07dcead
ss94866 <68229944+ss94866@users.noreply.github.com>
2020-10-10 13:29:24 +0530

    Update settings.py

--- imagereg/settings.py
+++ imagereg/settings.py
@@ -121,14 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
-
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
